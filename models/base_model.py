7f1e48bc51c7a1e34e4dc7844a5bb8e691fae722
LesLor <leslie.paz.am@gmail.com>
2022-07-07 22:18:30 +0000

    Fix: pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Constructor of BaseModel"""
-        self.id =  str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
@@ -23,11 +23,10 @@ class BaseModel():
         else:
             models.storage.new(self)
 
-
     def __str__(self):
         """This method return string representation"""
         class_name = type(self).__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return "[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """This method update attributte with time now"""
