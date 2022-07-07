75f6ebec4f47fe95f201b1622edb7e255b2b5631
LesLor <leslie.paz.am@gmail.com>
2022-07-07 21:46:41 +0000

    Feat: v1 BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -11,41 +11,31 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Constructor of BaseModel"""
-        self.id = str(uuid.uuid4())
+        self.id =  str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs is not None and len(kwargs) != 0:
-            for key in kwargs.keys():
-                if key != '__class__':
-                    setattr(self, key, kwargs[key])
-                    if key == "created_at":
-                        self.__dict__[key] = datetime.fromisoformat(
-                                                            kwargs[key])
-                    if key == "updated_at":
-                        self.__dict__[key] = datetime.fromisoformat(
-                                                            kwargs[key])
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                elif key != '__class__':
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
             models.storage.new(self)
+
 
     def __str__(self):
         """This method return string representation"""
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return f'[{type(self).__name__}]({self.id}) {self.__dict__}'
 
     def save(self):
         """This method update attributte with time now"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """This method return dictionary of all key/value of instance"""
         dict_new = self.__dict__.copy()
-
-        dict_new['__class__'] = self.__class__.__name__
-        formated_updated = dict_new['updated_at'].isoformat()
-        dict_new['updated_at'] = formated_updated
-        formated_created = dict_new['created_at'].isoformat()
-        dict_new['created_at'] = formated_created
+        dict_new["created_at"] = dict_new['created_at'].isoformat()
+        dict_new["updated_at"] = dict_new['updated_at'].isoformat()
+        dict_new["__class__"] = type(self).__name__
         return (dict_new)
