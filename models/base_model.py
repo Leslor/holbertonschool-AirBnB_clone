f1a70871bc176ac3cd6ed608cdd2b67626271a13
LesLor <leslie.paz.am@gmail.com>
2022-07-07 22:19:10 +0000

    Fix: pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel():
     def __str__(self):
         """This method return string representation"""
         class_name = type(self).__name__
-        return "[{class_name}] ({self.id}) {self.__dict__}"
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """This method update attributte with time now"""
